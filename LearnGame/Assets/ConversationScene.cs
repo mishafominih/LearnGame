ca08ddae4a604182b9126e6323889a27a8c82123
mishafominih <mishagjxn@mail.ru>
2021-04-09 23:13:17 +0500

    Создал проект, добавил главный экран

---
+++ LearnGame/Assets/ConversationScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ConversationScene : MonoBehaviour
+{
+    public string Scene;
+    void Start()
+    {
+        var btn = GetComponent<Button>();
+        if (btn == null) Debug.Log(name + "требует наличие кнопки для скрипта");
+        btn.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(Scene, LoadSceneMode.Single);
+        });
+    }
+}
