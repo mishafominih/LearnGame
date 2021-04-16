99ee12bb927ee15540fe23e53fee4a6299bfd3a4
yura159 <47238316+yura159@users.noreply.github.com>
2021-04-16 22:33:25 +0500

    Есть контакт
    Сконектиться получилось_

---
+++ LearnGame/Assets/Plagins/MongoDB/WorkMongoDB.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class WorkMogoDb : MonoBehaviour
+{ 
+
+    public static MongoClient client;
+    public static IMongoDatabase database;
+    public static void Connection()
+    {
+        client = new MongoClient("mongodb+srv://admin:0987654321@gameclaster.zzp3f.mongodb.net/GameClaster?retryWrites=true&w=majority");
+        database = client.GetDatabase("TestDatabase");
+    }
+
+    public static string GetData(string name, string path)
+    {
+        //GetImages(name, path);
+        return GetJson(name);
+    }
+
+    private static string GetJson(string name)
+    {
+        var collection = database.GetCollection<BsonDocument>("data_json");
+        var bson = new BsonDocument() { { "name", name } };
+        FilterDefinition<BsonDocument> filter = bson;
+        var jsonFormat = collection.Find(filter).FirstOrDefault().GetValue(2).ToJson();
+        return jsonFormat;
+    }
+
+    public static void GetImages(string name, string path)
+    {
+        var gridFS = new GridFSBucket(database);
+        var collection = database.GetCollection<BsonDocument>("data_image");
+        var filter = new BsonDocument() { { "father_name", name } };
+        var nameImages = new List<string>();
+        collection.FindSync(filter).ForEachAsync(x => nameImages.Add(x.GetValue(2).AsString));
+        foreach (var nameImage in nameImages)
+        {
+            var file = new FileStream(path + "\\" + nameImage, FileMode.OpenOrCreate);
+            gridFS.DownloadToStreamByNameAsync(nameImage, file);
+        }
+    }
+}
---
+++ LearnGame/Assets/Skripts/MongoDB/Connection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connection : MonoBehaviour
+{ 
+    void Start()
+    {
+        WorkMogoDb.Connection();
+        var result = WorkMogoDb.GetData("WhyId", @"D:\Example");
+    }
+}
